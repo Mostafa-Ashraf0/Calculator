0f7a5b7f7163940c4e0e5d559f8cf335c62318fd
Mostafa Ashraf <143865733+Mostafa-Ashraf0@users.noreply.github.com>
2025-06-02 20:39:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,38 +2,38 @@ let button = document.querySelectorAll("button");
 let screen = document.querySelector(".screen");
 let list = [];
 let equal = false;
-function startNewOperation() {
+function lastIsEqual() {
   if (equal) {
     screen.textContent = "";
     equal = false;
   }
-  function isNumber() {
-    if (!isNaN(list[list.length - 1]) && !isNaN(list[0])) {
-      let content = screen.textContent;
-      let result = eval(content);
-      screen.textContent = "";
-      screen.appendChild(document.createTextNode(result));
-    } else {
-      screen.textContent = "undefined";
-    }
-  }
+}
+
+function isDefined() {
+  let content = screen.textContent;
+  let result = eval(content);
+  screen.textContent = "";
+  screen.appendChild(document.createTextNode(result));
 }
 button.forEach((e) =>
   e.addEventListener("click", (e) => {
-    startNewOperation(equal);
     let node = document.createTextNode(e.target.dataset.value);
+    lastIsEqual();
     // not push = in the list
-    if (e.target.dataset.value !== "=") {
-      list.push(e.target.dataset.value);
-    }
+
     if (e.target.dataset.value === "=") {
       equal = true;
       //if the last input is operator do nothing
-      isNumber();
+      if (!isNaN(list[list.length - 1]) && !isNaN(list[0])) {
+        isDefined();
+      } else {
+        screen.textContent = "undefined";
+      }
     } else if (e.target.dataset.value === "C") {
       screen.textContent = "";
       list = [];
     } else {
+      list.push(e.target.dataset.value);
       screen.appendChild(node);
     }
   })
